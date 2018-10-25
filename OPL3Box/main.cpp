547293aa327b8b808195d1081f622e4cc32ad698
Antonio Willy Malara <antonio.malara@gmail.com>
2018-10-25 17:12:05 +0200

    Initial Commit

---
+++ OPL3Box/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OPL3Box
+//
+//  Created by Antonio Malara on 25/10/2018.
+//  Copyright © 2018 Antonio Malara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
